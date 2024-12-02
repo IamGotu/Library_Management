7547df6add00938027b473fc1e96cc2b0d322a39
Mark John <markjohnjopia1@gmail.com>
2024-12-02 17:02:56 +0800

    navbar dropdown and some minor changes

--- borrower/navbar.php
+++ borrower/navbar.php
@@ -1,11 +1,7 @@
 <!-- Navigation Bar -->
 <nav class="navbar">
-    <a class="navbar-brand" href="#">Library Management</a>
+    <a class="navbar-brand" href="#">Library Management - <?php echo htmlspecialchars($_SESSION['user_type'], ENT_QUOTES, 'UTF-8'); ?></a>
     <ul class="nav">
-        <!-- User Name and Profile -->
-        <li class="nav-item">
-            <a class="nav-link" href="profile.php"><?php echo htmlspecialchars($_SESSION['user_name'], ENT_QUOTES, 'UTF-8'); ?></a>
-        </li>
         <!-- Navigation Links -->
         <li class="nav-item">
             <a class="nav-link" href="view.php">Search Resources</a>
@@ -16,8 +12,16 @@
         <li class="nav-item">
             <a class="nav-link" href="borrow_history.php">Borrow History</a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link logout-link" href="../login/logout.php">Logout</a>
+        <!-- User Name and Profile -->
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <?php echo htmlspecialchars($_SESSION['user_name'], ENT_QUOTES, 'UTF-8'); ?>
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="userDropdown">
+                <li><a class="dropdown-item" href="profile.php">Profile</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="../login/logout.php">Logout</a></li>
+            </ul>
         </li>
     </ul>
 </nav>
