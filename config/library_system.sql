4c21edd29d89f832008d4099fd37fef73d8ca0f9
Mark John <markjohnjopia1@gmail.com>
2024-12-10 13:19:06 +0800

    database sql

--- config/library_system.sql
+++ config/library_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2024 at 06:12 PM
+-- Generation Time: Dec 10, 2024 at 06:18 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -49,7 +49,9 @@ INSERT INTO `books` (`BookID`, `Title`, `Author`, `ISBN`, `Publisher`, `Edition`
 (78, '', 'Harper Lee', '978-0061120084', 'J.B. Lippincott & Co.', NULL, '1960-07-11', NULL, 'available'),
 (79, '', 'Patrick M. Fitzpatrick', '978-0534376034', 'Brooks Cole', NULL, '2002-01-01', NULL, 'available'),
 (80, '', 'American Psychological Association', '978-1433832176', 'APA Publishing', NULL, '2020-12-15', NULL, 'available'),
-(81, '', 'Yuval Noah Harari', '978-0062316097', 'Harper', NULL, '2015-02-10', NULL, 'available');
+(81, '', 'Yuval Noah Harari', '978-0062316097', 'Harper', NULL, '2015-02-10', NULL, 'available'),
+(124, '', 'adsad', 'aada', 'asdad', NULL, '2024-12-10', NULL, 'available'),
+(158, '', '11', '11', '11', NULL, '2024-12-11', NULL, 'available');
 
 -- --------------------------------------------------------
 
@@ -92,7 +94,18 @@ INSERT INTO `borrow_transactions` (`ID`, `BorrowerID`, `Borrower_first_name`, `B
 (29, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 78, 'Book', 'B-2024-001', '2024-12-09', '2024-12-23', '2024-12-09', 'returned'),
 (30, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 79, 'Book', 'B-2024-002', '2024-12-09', '2024-12-23', '2024-12-09', 'returned'),
 (31, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 78, 'Book', 'B-2024-001', '2024-12-09', '2024-12-23', '2024-12-09', 'returned'),
-(34, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 118, 'Periodical', 'P-2024-001', '2024-10-09', '2024-10-23', '2024-12-09', 'returned');
+(34, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 118, 'Periodical', 'P-2024-001', '2024-10-09', '2024-10-23', '2024-12-09', 'returned'),
+(35, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 78, 'Book', 'B-2024-001', '2024-12-09', '2024-12-23', '2024-12-09', 'returned'),
+(36, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 102, 'MediaResource', 'R-2024-001', '2024-11-09', '2024-11-23', '2024-12-09', 'returned'),
+(37, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 107, 'MediaResource', 'R-2024-002', '2024-12-09', '2024-12-23', '2024-12-09', 'returned'),
+(38, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 80, 'Book', 'B-2024-003', '2024-12-09', '2024-12-23', '0000-00-00', 'returned'),
+(39, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 78, 'Book', 'B-2024-001', '2024-12-06', '2024-12-20', '2024-12-09', 'returned'),
+(40, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 78, 'Book', 'B-2024-001', '2024-12-06', '2024-12-20', '2024-12-09', 'returned'),
+(41, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 78, 'Book', 'B-2024-001', '2024-11-24', '2024-12-08', '2024-12-09', 'returned'),
+(42, 3003212, 'faculty', 'faculty', 'faculty', '', 2007136, 'staff', 'staff', 'staff', '', 79, 'Book', 'B-2024-002', '2024-12-09', '2024-12-23', '2024-12-09', 'returned'),
+(43, 3003212, 'faculty', 'faculty', 'faculty', '', 2007136, 'staff', 'staff', 'staff', '', 79, 'Book', 'B-2024-002', '2024-11-24', '2024-12-20', '2024-12-09', 'returned'),
+(44, 3003212, 'faculty', 'faculty', 'faculty', '', 2007136, 'staff', 'staff', 'staff', '', 79, 'Book', 'B-2024-002', '2024-11-24', '2024-12-08', '2024-12-09', 'returned'),
+(45, 3003212, 'faculty', 'faculty', 'faculty', '', 2007136, 'staff', 'staff', 'staff', '', 107, 'MediaResource', 'R-2024-002', '2024-12-09', '2024-12-23', NULL, 'borrowed');
 
 -- --------------------------------------------------------
 
@@ -108,23 +121,27 @@ CREATE TABLE `fines` (
   `Borrower_middle_name` varchar(255) DEFAULT NULL,
   `Borrower_last_name` varchar(255) NOT NULL,
   `Borrower_suffix` varchar(255) DEFAULT NULL,
-  `ApproverID` int(100) NOT NULL,
-  `Approver_first_name` varchar(255) NOT NULL,
+  `ApproverID` int(100) DEFAULT NULL,
+  `Approver_first_name` varchar(255) DEFAULT NULL,
   `Approver_middle_name` varchar(255) DEFAULT NULL,
-  `Approver_last_name` varchar(255) NOT NULL,
+  `Approver_last_name` varchar(255) DEFAULT NULL,
   `Approver_suffix` varchar(100) DEFAULT NULL,
   `Amount` decimal(10,2) NOT NULL,
   `DateGenerated` date NOT NULL DEFAULT curdate(),
   `DatePaid` date DEFAULT NULL,
-  `PaidStatus` enum('unpaid','paid') NOT NULL DEFAULT 'unpaid'
+  `PaidStatus` enum('unpaid','paid') NOT NULL DEFAULT 'unpaid',
+  `ReceiptPrinted` enum('no','yes') NOT NULL DEFAULT 'no'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `fines`
 --
 
-INSERT INTO `fines` (`ID`, `BorrowTransactionID`, `BorrowerID`, `Borrower_first_name`, `Borrower_middle_name`, `Borrower_last_name`, `Borrower_suffix`, `ApproverID`, `Approver_first_name`, `Approver_middle_name`, `Approver_last_name`, `Approver_suffix`, `Amount`, `DateGenerated`, `DatePaid`, `PaidStatus`) VALUES
-(7, 34, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 2350.00, '2024-12-10', '2024-12-09', 'paid');
+INSERT INTO `fines` (`ID`, `BorrowTransactionID`, `BorrowerID`, `Borrower_first_name`, `Borrower_middle_name`, `Borrower_last_name`, `Borrower_suffix`, `ApproverID`, `Approver_first_name`, `Approver_middle_name`, `Approver_last_name`, `Approver_suffix`, `Amount`, `DateGenerated`, `DatePaid`, `PaidStatus`, `ReceiptPrinted`) VALUES
+(7, 34, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 2350.00, '2024-12-10', '2024-12-09', 'paid', 'no'),
+(11, 36, 4008485, 'student', 'student', 'student', '', 2007136, 'staff', 'staff', 'staff', '', 800.00, '2024-12-10', '2024-12-09', 'paid', 'no'),
+(12, 41, 4008485, 'student', 'student', 'student', '', NULL, NULL, NULL, NULL, '', 50.00, '2024-12-10', NULL, 'unpaid', 'no'),
+(13, 44, 3003212, 'faculty', 'faculty', 'faculty', '', 2007136, 'staff', 'staff', 'staff', '', 50.00, '2024-12-10', '2024-12-09', 'paid', 'no');
 
 -- --------------------------------------------------------
 
@@ -146,12 +163,12 @@ CREATE TABLE `libraryresources` (
 --
 
 INSERT INTO `libraryresources` (`ResourceID`, `Title`, `AccessionNumber`, `Category`, `ResourceType`, `AvailabilityStatus`) VALUES
-(78, 'To Kill a Mockingbird', 'B-2024-001', 'Fiction', 'Book', 'Available'),
-(79, 'Advanced Calculus', 'B-2024-002', 'Academic', 'Book', 'Available'),
+(78, 'To Kill a Mockingbird', 'B-2024-001', 'Fiction', 'Book', 'Checked Out'),
+(79, 'Advanced Calculus', 'B-2024-002', 'Academic', 'Book', 'Checked Out'),
 (80, 'APA Handbook of Psychology', 'B-2024-003', 'Reference', 'Book', 'Available'),
 (81, 'Sapiens: A Brief History of Humankind', 'B-2024-004', 'Non-Fiction', 'Book', 'Available'),
-(102, 'Harry Potter and the Sorcerer\'s Stone', 'R-2024-001', 'AudioBook', 'MediaResource', 'Available'),
-(107, 'The Shawshank Redemption', 'R-2024-002', 'Film', 'MediaResource', 'Available'),
+(102, 'Harry Potter and the Sorcerer\'s Stone', 'R-2024-001', 'AudioBook', 'MediaResource', 'Checked Out'),
+(107, 'The Shawshank Redemption', 'R-2024-002', 'Film', 'MediaResource', 'Checked Out'),
 (108, 'Thriller by Michael Jackson', 'R-2024-003', 'Music', 'MediaResource', 'Available'),
 (109, 'The Godfather Trilogy', 'R-2024-004', 'Film', 'MediaResource', 'Available'),
 (118, 'The Global Times', 'P-2024-001', 'Newspaper', 'Periodical', 'Checked Out'),
@@ -179,8 +196,8 @@ CREATE TABLE `mediaresources` (
 --
 
 INSERT INTO `mediaresources` (`MediaResourceID`, `ResourceID`, `Format`, `Runtime`, `MediaType`) VALUES
-(32, 102, 'Digital', '8 hours 17 minutes', 'AudioBook'),
-(33, 107, 'DVD', '142 minutes', 'Film'),
+(32, 102, 'DVD', '8 hours 17 minutes', 'AudioBook'),
+(33, 107, 'Digital', '142 minutes', 'Film'),
 (34, 108, 'Blu-ray', '42 minutes', 'Music'),
 (35, 109, 'VHS', '537 minutes', 'Film');
 
@@ -246,7 +263,8 @@ INSERT INTO `users` (`id`, `first_name`, `middle_name`, `last_name`, `suffix`, `
 (1, 'admin', 'admin', 'admin', '', 'admin@gmail.com', 'admin', NULL, '2001-06-14', 'Brgy. Sinawal', 'Prk. Lansang Village', 'Brgy. Sinawal', 'General Santos City', '09514810354', 'active', '1003679', '$2y$10$CUJzdLvfCpQZLenJ/gW.Z.G6Xiu8dNnLwsIOFcxgq9NceznjOustK', '2024-12-02 13:48:00', '2024-12-09 10:51:37'),
 (2, 'staff', 'staff', 'staff', '', 'staff@gmail.com', 'staff', NULL, '2001-06-14', 'Blck. 3', 'Prk. Lansang Village', 'Brgy. Sinawal', 'General Santos City', '09514810354', 'active', '2007136', '$2y$10$ueFwbuMC55wyCGnSllj6A.b2ILKnYoHWSfu07UosCZMHGfYQLDB/O', '2024-11-25 12:53:20', '2024-12-09 10:51:53'),
 (3, 'faculty', 'faculty', 'faculty', '', 'faculty@gmail.com', 'faculty', NULL, '2001-06-14', 'Brgy. Sinawal', 'Prk. Lansang Village', 'Brgy. Sinawal', 'General Santos City', '09514810354', 'active', '3003212', '$2y$10$Tp2aiMpMjulz/6iGGYaPIu8AyrOEjR1dcgoXGGx9nGbu3OCvQIV..', '2024-12-02 13:50:18', '2024-12-09 10:52:10'),
-(4, 'student', 'student', 'student', '', 'student@gmail.com', 'student', NULL, '2001-06-14', 'Blck. 3', 'Prk. Lansang Village', 'Brgy. Sinawal', 'General Santos City', '09514810354', 'active', '4008485', '$2y$10$qg8olo7U0gEOvuBUsktOUeW5dQgy36EyD0A5q.Okzd1XQRgk6M/da', '2024-11-25 12:55:45', '2024-12-09 10:52:20');
+(4, 'student', 'student', 'student', '', 'student@gmail.com', 'student', NULL, '2001-06-14', 'Blck. 3', 'Prk. Lansang Village', 'Brgy. Sinawal', 'General Santos City', '09514810354', 'active', '4008485', '$2y$10$qg8olo7U0gEOvuBUsktOUeW5dQgy36EyD0A5q.Okzd1XQRgk6M/da', '2024-11-25 12:55:45', '2024-12-09 10:52:20'),
+(15, 'Mark John', 'Rama', 'Jopia', '', 'markjohnjopia1@gmail.com', 'admin', NULL, '2001-06-14', 'Block 3', NULL, 'Brgy. Sinawal', 'General Santos City', '09514810354', 'active', '1004956', '$2y$10$ZYAuCFDkiW1DynS.2dx.TexkKlAp2ERMC5ik1ihHDPlW/kx3cq4Ne', '2024-12-10 01:37:07', '2024-12-10 01:37:07');
 
 --
 -- Indexes for dumped tables
@@ -273,7 +291,6 @@ ALTER TABLE `borrow_transactions`
 --
 ALTER TABLE `fines`
   ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `BorrowID` (`BorrowerID`),
   ADD KEY `BorrowTransactionID` (`BorrowTransactionID`);
 
 --
@@ -314,43 +331,43 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `BookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+  MODIFY `BookID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=159;
 
 --
 -- AUTO_INCREMENT for table `borrow_transactions`
 --
 ALTER TABLE `borrow_transactions`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `fines`
 --
 ALTER TABLE `fines`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `libraryresources`
 --
 ALTER TABLE `libraryresources`
-  MODIFY `ResourceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+  MODIFY `ResourceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=159;
 
 --
 -- AUTO_INCREMENT for table `mediaresources`
 --
 ALTER TABLE `mediaresources`
-  MODIFY `MediaResourceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `MediaResourceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `periodicals`
 --
 ALTER TABLE `periodicals`
-  MODIFY `PeriodicalID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `PeriodicalID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
@@ -360,8 +377,8 @@ ALTER TABLE `users`
 -- Constraints for table `fines`
 --
 ALTER TABLE `fines`
-  ADD CONSTRAINT `borrower_ibfk_1` FOREIGN KEY (`BorrowerID`) REFERENCES `borrow_transactions` (`BorrowerID`),
-  ADD CONSTRAINT `fines_ibfk_1` FOREIGN KEY (`BorrowTransactionID`) REFERENCES `borrow_transactions` (`ID`);
+  ADD CONSTRAINT `borrower_ibfk_1` FOREIGN KEY (`BorrowerID`) REFERENCES `borrow_transactions` (`BorrowerID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fines_ibfk_1` FOREIGN KEY (`BorrowTransactionID`) REFERENCES `borrow_transactions` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `mediaresources`
